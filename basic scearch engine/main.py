e73a44c45d53dfad2b788ceff5b2ccff6630e33d
J-4-S-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-08-18 22:51:07 +0800

    Add files via upload

---
+++ basic scearch engine/main.py
@@ -0,0 +1,46 @@
+
+class stack():
+    def __init__(self):
+        self.list = []
+        # self.list = {
+        #     "banana" : "5$",
+        #     "apple" : "4$",
+        #     "flower" : "3$",
+        #     "nail" : "4$",
+        #     "hammer" : "10$"
+        # }
+
+    # def push(self, item):
+    #     list.append(item)
+
+    def pop(self, item):
+        for i in self.list():
+            if item == i:
+                pos = self.list("i")
+                self.list.pop(pos)
+
+def scearch():
+    for i in lis():
+        if inp == i:
+            new_stack.pop(i)
+            print(i)
+            break
+        
+
+def main():
+    new_stack = stack()
+    while True:
+        print("*please enter it correctly")
+        print("options : ")
+        for a in lis():
+            print(a)
+
+        global inp
+        inp = input("enter >> ")
+        scearch()
+
+
+        
+
+if __name__ == "__main__":
+    pass
